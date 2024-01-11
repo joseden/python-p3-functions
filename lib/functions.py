03508d54e258f053db981f55651c04f49a33b55d
Joseph <joseph.dena@student.moringaschool.com>
2024-01-11 21:32:29 +0300

    Complete Assignment
    Signed-off-by: Joseph <joseph.dena@student.moringaschool.com>

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
-
-def greet(name):
-    pass
+    print (f"Hello, programmer!")
+    
+def greet(name="Naureen"):
+    print(f"Hello, {name}!")
+    
+greet()
+   
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+   
+greet_with_default()
+
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+num1 = 45
+num2 = 55
+
+result = add(num1, num2)
+print(result)
+
 
 def halve(number):
-    pass
+    return number // 2
+
+number = 100
+result = halve(number)
+print(result)
+
+
+def halve(number):
+    return number / 2
+
+number = 100
+result = halve (number)
+print(result)
+
+
+
